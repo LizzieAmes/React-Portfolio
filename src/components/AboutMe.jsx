b8cc32be92745daad0b5604a32f8d7fb175a20c3
Lizzie <lizziejacoboo@gmail.com>
2024-03-20 16:34:24 -0600

    added logo's, background image, linked contact me button on about me page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import './AboutMe.css'; 
+import './AboutMe.css';
+import { Link } from 'react-router-dom';
+import planetImage from '../assets/images/planet.png';
 
 const AboutMe = () => {
   return (
-    <div className="about-me">
-      <h1>Hi, my name is</h1>
-      <h2>Lizzie Ames</h2>
-      <h3>Full Stack Developer🚀</h3>
-      <p>
-        Welcome to my portfolio! I'm a passionate web developer with
-        experience in building web applications from the ground up. I love tackling challenges and learning new technologies.
-      </p>
-      <button>Contact Me </button>
+    <div
+      className="about-me"
+      style={{
+        backgroundImage: `url(${planetImage})`,
+        backgroundSize: '650px 650px',
+        backgroundPosition: '80% center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className="text-container">
+        <h1>Hi, my name is</h1>
+        <h2>Lizzie Ames</h2>
+        <h3>Full Stack Developer</h3>
+        <p>
+          Welcome to my portfolio! I'm a passionate web developer with
+          experience in building web applications from the ground up. I love
+          tackling challenges and learning new technologies.
+        </p>
+        <Link to="/contact" className="contact-me-button">
+          Contact Me
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Footer.css'; 
 import githubLogo from '../assets/images/github.png';
+import linkedinLogo from '../assets/images/linkedin.png';
 
 const Footer = () => (
   <footer className="footer">
@@ -8,17 +9,15 @@ const Footer = () => (
       href="https://github.com/LizzieAmes"
       target="_blank"
       rel="noopener noreferrer"
-      
     >
-      <img src="https://github.com/LizzieAmes/Lizzie-Ames-Professional-Portfolio/blob/main/assets/images/github.png" alt="GitHub" style={{width: '24px', height: '24px'}} />
-      GitHub
+      <img src={githubLogo} alt="GitHub" className="social-icons" />
     </a>
     <a
       href="https://www.linkedin.com/in/elizabeth-ames-2844671a6/"
       target="_blank"
       rel="noopener noreferrer"
     >
-      LinkedIn
+      <img src={linkedinLogo} alt="LinkedIn" className="social-icons" />
     </a>
   </footer>
 );
